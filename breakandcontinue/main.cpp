c04147873fc0a46753e9c9a9a7b8fa5b9ffd42a3
sejall <33982474+sejall@users.noreply.github.com>
2018-08-13 07:22:55 +0530

    Add files via upload

---
+++ breakandcontinue/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=0;i<=10;i++)
+    {
+        if(i==4)
+            break;
+        else
+            cout<<i<<endl;
+    }
+    cout<<"hello"<<endl;
+    return 0;
+}
